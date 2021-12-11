c38ec10317bb01637f108094a1360c0a3ded2064
Pandey <mp484v@intl.att.com>
2021-12-11 23:28:34 +0530

    latest

--- model.py
+++ model.py
@@ -56,7 +56,7 @@ class Recommendation:
         #mdl=pd.read_pickle('final_lr.pkl')
         #features = pickle.load(open('features.pkl','rb'))
         #vectorizer = TfidfVectorizer(vocabulary = features)
-        items=items.index.get_level_values('productId').unique().tolist()
+        items=items.get_level_values('productId').unique().tolist()
         print(items)
         temp=self.data[self.data.id.isin(items)]
         print(temp)
